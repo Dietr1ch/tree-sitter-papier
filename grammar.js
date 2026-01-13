62422895413f8c648e63e64f72b7ae473778218e
Dietrich Daroch <Dietrich@Daroch.me>
2026-01-13 16:22:18 -0300

    Add draft grammar

--- grammar.js
+++ grammar.js
@@ -7,11 +7,106 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
+const NL = /\n/;
+const SPACE = /[\s]+/; // [[:space:]] /[\t\n\v\f\r ]/
+const TAB = /\t/;
+const SPACE_TAB = /[[:blank:]]+/; // [\t ]
+
+const REGEX_LINE = /[^\n]+/;
+const REGEX_CHUNK = /[^ \t\n]+/;
+
+const REGEX_WORD = REGEX_CHUNK;
+const REGEX_PUNCTUATION = /[.,;:!?]/; // TODO: Consider /[[:punct:]]/
+
+const REGEX_UUID =
+  /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
+const REGEX_ALIAS = /[a-zA-Z][-_a-zA-Z0-9]*/;
+const REGEX_REF = REGEX_CHUNK;
+const REGEX_TAG = new RustRegex('(?i)[a-z0-9][-_a-z0-9]*');
+const REGEX_FMT = /[a-z][-_a-z0-9]*/;
+
+/**
+ * Implements Elem+ using a separator.
+ * @param {RuleOrLiteral} elem
+ * @param {string} sep
+ */
+function many_sep(elem, sep) {
+  return seq(elem, repeat(seq(sep, elem)));
+}
+
+/**
+ * A prefix-defined token (#tag, @at, /cmd, $VAR)
+ * @param {string} name
+ * @param {int} precedence
+ * @param {RuleOrLiteral} prefix
+ * @param {RuleOrLiteral} pattern
+ */
+function prefix_token(name, precedence, prefix, pattern) {
+  return token(prec(precedence, seq(prefix, field(name, pattern))));
+}
+
+// NOTE: This is under development and does not even work.
 module.exports = grammar({
-  name: "papier",
+  name: 'papier',
+
+  extras: _ => [
+    // NOTE: No \n
+    ' ',
+    '\r', // fuck this shit!
+  ],
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello",
+    papier: $ => repeat(choice(prec(1, $.sub_document), $.text)),
+
+    // Text
+    // ----
+    text: $ => prec.right(repeat1($.line)),
+    line: $ => seq(optional(repeat1($.word)), NL),
+
+    word: $ =>
+      seq(
+        choice(
+          $.uuid,
+          $.alias,
+          $.ref,
+          $.tag,
+          $.fmt,
+
+          REGEX_WORD,
+        ),
+        optional(REGEX_PUNCTUATION),
+      ),
+    uuid: $ => prefix_token('uuid', 2, '!', REGEX_UUID),
+    alias: $ => prefix_token('alias', 1, '!!', REGEX_ALIAS),
+    ref: $ => prefix_token('ref', 1, '!?', REGEX_REF),
+    tag: $ => prefix_token('tag', 1, '#', REGEX_TAG),
+    fmt: $ => prefix_token('format', 1, '@', REGEX_FMT),
+
+    // Sub-document
+    // ------------
+    // `^# (?P<title>) {$`
+    // `\t(?P<line>)$`
+    // `^}$`
+    sub_document: $ =>
+      prec.left(
+        seq(
+          '* ',
+          optional(field('title', repeat1($.word))),
+          '{',
+          optional(field('contents', $.contents)),
+          '}',
+          optional(NL),
+        ),
+      ),
+
+    contents: $ =>
+      repeat1(
+        field(
+          'line', //
+          choice($._empty_line, $._content_line),
+        ),
+      ),
+    _empty_line: $ => NL,
+    _content_line: $ => seq('\t', REGEX_LINE),
   },
 });
---
+++ queries/highlights.scm
@@ -0,0 +1,47 @@
+;; Highlights
+
+;; Docs
+;; - Queries: https://tree-sitter.github.io/tree-sitter/using-parsers/queries/index.html
+
+
+;; Links (Identifiers)
+
+;; Words
+(uuid) @constant.builtin
+(alias) @module
+(ref) @constant
+(tag) @tag
+(fmt) @type
+(word) @comment
+
+(sub_document) @function
+(contents) @attribute
+
+
+;; Highlighting classes:
+;; - attribute
+;; - comment
+;; - constant
+;; - constant.builtin
+;; - constructor
+;; - embedded
+;; - function
+;; - function.builtin
+;; - keyword
+;; - module
+;; - number
+;; - operator
+;; - property
+;; - property.builtin
+;; - punctuation
+;; - punctuation.bracket
+;; - punctuation.delimiter
+;; - punctuation.special
+;; - string
+;; - string.special
+;; - tag
+;; - type
+;; - type.builtin
+;; - variable
+;; - variable.builtin
+;; - variable.parameter
